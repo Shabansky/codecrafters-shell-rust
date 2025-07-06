d3363ba152345c837da740d900720eabbb61b826
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-07-06 09:08:36 +0300

    FTR: Update REPL handles signatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-fn read() {}
-fn eval() {}
-fn print() {}
+struct Expression;
+
+fn read(input: &str) -> Expression {
+    Expression
+}
+
+fn eval(expression: Expression) -> String {
+    todo!()
+}
+
+fn print(output: String) {}
