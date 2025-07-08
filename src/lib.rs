710862d1fe5f3e6eab9d4dc673dc7dcbe7818a97
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-07-08 20:49:46 +0300

    FTR: Add support for exit builtin with code of 0

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,35 @@
+use std::process;
+struct BuiltinExit {
+    code: i32,
+}
+
+impl BuiltinExit {
+    fn from(expression: &Expression) -> Self {
+        Self { code: 0 }
+    }
+
+    fn run(&self) {
+        process::exit(self.code);
+    }
+}
+
 pub struct Expression {
     input: String,
 }
 
 pub fn read(input: String) -> Expression {
-    Expression { input }
+    //TODO: .to_string() is overkill. Consider input to be of type &str instead
+    Expression {
+        input: input.trim().to_string(),
+    }
 }
 
 pub fn eval(expression: Expression) -> String {
+    println!("{}", expression.input);
+    //TODO: Below needs to be split and create BuiltinExit properly
+    if expression.input == "exit 0" {
+        BuiltinExit::from(&expression).run();
+    }
     format!("{}: command not found", expression.input.trim())
 }
 
