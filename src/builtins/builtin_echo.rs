342b6381c22b046d0741dc32d0904f26d1eb77c9
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-07-14 15:36:00 +0300

    TST: Update unit tests

--- src/builtins/builtin_echo.rs
+++ src/builtins/builtin_echo.rs
@@ -13,3 +13,18 @@ impl BuiltinEcho {
         format!("{}", self.text)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::expression::Expression;
+
+    #[test]
+    fn echo_returns_own_input() {
+        let input = String::from("echo my test input");
+        let expression = Expression::from(input);
+        let expected_output = expression.arguments.join(" ").clone();
+        let output = BuiltinEcho::from(expression).run();
+        assert_eq!(expected_output, output);
+    }
+}
--- src/builtins/builtin_exit.rs
+++ src/builtins/builtin_exit.rs
@@ -21,3 +21,69 @@ impl BuiltinExit {
         process::exit(self.code);
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn return_builtin_on_input(input: String) -> BuiltinExit {
+        let expression = Expression::from(input);
+        BuiltinExit::from(expression)
+    }
+
+    #[test]
+    fn no_exit_code_returns_zero() {
+        let input = String::from("exit");
+        let builtin = return_builtin_on_input(input);
+
+        assert_eq!(0, builtin.code);
+    }
+
+    #[test]
+    fn zero_exit_code_returns_zero() {
+        let input = String::from("exit 0");
+        let builtin = return_builtin_on_input(input);
+
+        assert_eq!(0, builtin.code);
+    }
+
+    #[test]
+    fn negative_exit_code_returns_self() {
+        let input = String::from("exit -1");
+        let builtin = return_builtin_on_input(input);
+
+        assert_eq!(-1, builtin.code);
+    }
+
+    #[test]
+    fn positive_exit_code_returns_self() {
+        let input = String::from("exit 1");
+        let builtin = return_builtin_on_input(input);
+
+        assert_eq!(1, builtin.code);
+    }
+
+    #[test]
+    fn non_number_returns_zero() {
+        let input = String::from("exit incorrect");
+        let builtin = return_builtin_on_input(input);
+
+        assert_eq!(0, builtin.code);
+    }
+
+    #[test]
+    fn multiple_arguments_return_zero() {
+        let input = String::from("exit something here");
+        let builtin = return_builtin_on_input(input);
+
+        assert_eq!(0, builtin.code);
+    }
+
+    #[test]
+    fn multiple_arguments_with_numeral_return_zero() {
+        let input = String::from("exit something 1");
+        let builtin = return_builtin_on_input(input);
+
+        assert_eq!(0, builtin.code);
+    }
+}
